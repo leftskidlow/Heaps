539e778117dfe3a9ea890bdc0019cb852dcb02c5
leftskidlow <32857018+leftskidlow@users.noreply.github.com>
2022-03-07 12:48:56 -0500

    Exercise 3

--- Heaps.playground/Pages/Step 3 - Adding Elements.xcplaygroundpage/Contents.swift
+++ Heaps.playground/Pages/Step 3 - Adding Elements.xcplaygroundpage/Contents.swift
@@ -1,74 +1,77 @@
-//import Foundation
-//
-//class MinHeap {
-//
-//    // Instance Variables
-//    private var heap: [TaskNode]
-//    private var size: Int {
-//        heap.count
-//    }
-//
-//    // Initializer
-//    init() {
-//        self.heap = [];
-//    }
-//
-//    // Public Methods
-//
-//    // Add Method -> Adds a task to the heap
-//
-//
-//
-//    // Private Methods
-//
-//    // HeapifyUp Method -> Makes sure the the parent node is older than its children
-//}
-//
-//class TaskNode {
-//
-//    // Instance Variables
-//    private var dueDate: Date
-//    private var task: String
-//
-//    // Initializer
-//    init(_ task: String, _ dueDate: Date) {
-//        self.dueDate = dueDate
-//        self.task = task
-//    }
-//
-//    // Getter Methods
-//    func getTask() -> String {
-//        return task
-//    }
-//}
-//
-//extension MinHeap: CustomStringConvertible {
-//
-//    var description: String {
-//        var text = ""
-//        var taskNumber = 1
-//        for task in heap {
-//            text += "\(taskNumber): \(task)\n"
-//            taskNumber += 1
-//        }
-//        return String(text.dropLast(2))
-//    }
-//}
-//extension TaskNode: Comparable, CustomStringConvertible {
-//    static func < (lhs: TaskNode, rhs: TaskNode) -> Bool {
-//        lhs.dueDate < rhs.dueDate
-//    }
-//
-//    static func == (lhs: TaskNode, rhs: TaskNode) -> Bool {
-//        lhs.dueDate == rhs.dueDate && lhs.task == rhs.task
-//    }
-//    var description: String {
-//        if dueDate < Date.now {
-//            return "LATE: \(task), Due: \(dueDate.formatted())"
-//        }
-//        return "\(task), Due: \(dueDate.formatted())"
-//    }
-//}
+import Foundation
+
+class MinHeap {
+
+    // Instance Variables
+    private var heap: [TaskNode]
+    private var size: Int {
+        heap.count
+    }
+
+    // Initializer
+    init() {
+        self.heap = [];
+    }
+
+    // Public Methods
+
+    // Add Method -> Adds a task to the heap
+
+
+
+    // Private Methods
+
+    // HeapifyUp Method -> Makes sure the the parent node is older than its children
+}
+
+class TaskNode {
+
+    // Instance Variables
+    private var dueDate: Date
+    private var task: String
+
+    // Initializer
+    init(_ task: String, _ dueDate: Date) {
+        self.dueDate = dueDate
+        self.task = task
+    }
+
+    // Getter Methods
+    func getTask() -> String {
+        return task
+    }
+}
+
+extension MinHeap: CustomStringConvertible {
+
+    var description: String {
+        var text = ""
+        var taskNumber = 1
+        for task in heap {
+            text += "\(taskNumber): \(task)\n"
+            taskNumber += 1
+        }
+        return String(text.dropLast(2))
+    }
+}
+extension TaskNode: Comparable, CustomStringConvertible {
+    static func < (lhs: TaskNode, rhs: TaskNode) -> Bool {
+        lhs.dueDate < rhs.dueDate
+    }
+
+    static func == (lhs: TaskNode, rhs: TaskNode) -> Bool {
+        lhs.dueDate == rhs.dueDate && lhs.task == rhs.task
+    }
+    var description: String {
+        if dueDate < Date.now {
+            return "LATE: \(task), Due: \(dueDate.formatted())"
+        }
+        return "\(task), Due: \(dueDate.formatted())"
+    }
+}
+
+/* Final Save Point
+ 
 import Foundation
 
 class MinHeap {
@@ -87,12 +90,20 @@ class MinHeap {
     // Public Methods
     
     // Add Method -> Adds a task to the heap
-    
+    func add(task: String, dueDate: Date) {
+        let taskNode = TaskNode(task, dueDate)
+        print("Adding [\(taskNode.getTask())]...")
+        heap.append(taskNode)
+        heapifyUp()
+    }
     
     
     // Private Methods
     
     // HeapifyUp Method -> Makes sure the the parent node is older than its children
+    private func heapifyUp() {
+        
+    }
 }
 
 class TaskNode {
@@ -140,3 +151,4 @@ extension TaskNode: Comparable, CustomStringConvertible {
         return "\(task), Due: \(dueDate.formatted())"
     }
 }
+*/
